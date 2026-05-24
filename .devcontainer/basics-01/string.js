f9f915eaf57b17058e2d1fb5665abeb841764ff8
mishtyvaid <168573988+mishtyvaid@users.noreply.github.com>
2026-05-24 20:33:05 +0000

    string operators

--- .devcontainer/basics-01/string.js
+++ .devcontainer/basics-01/string.js
@@ -2,3 +2,12 @@ const name='hehehe'
 const id=78
 console.log(name+id+" waooowww")
 console.log(`my name is ${name} and id is ${id}`)
+const reaction= new String ('WWWaoowwwwwWW!!')
+console.log(reaction[2])
+console.log(reaction[0])
+console.log(reaction.length)
+console.log(reaction.toLowerCase())
+console.log(reaction.charAt(13))
+console.log(reaction.indexOf('!'))
+console.log(reaction.substring(10,14)) //negative se start ni hota
+console.log(reaction.slice(-15,4)) 
