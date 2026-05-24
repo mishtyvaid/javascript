b51e49dd5b62182c2d69e0306d2eff789b581683
mishtyvaid <168573988+mishtyvaid@users.noreply.github.com>
2026-05-24 19:26:20 +0000

    conversions

---
+++ .devcontainer/basics-01/conversions.js
@@ -0,0 +1,18 @@
+// conversion
+
+let id ="123"
+let num=Number(id)
+console.log(typeof(num))
+
+//operations
+
+console.log(2+2)
+console.log(2*2)
+console.log(2**3) //power
+console.log(3/2) //divide
+console.log(8%3) //remainder
+
+let str1="hello"
+let str2=" hehehe"
+let str3=str1+str2
+console.log(str3)
